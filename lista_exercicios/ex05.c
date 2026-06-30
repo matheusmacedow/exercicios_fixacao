bac0a34743a6a6f70b1157bef0b1353128f9bed6
Matheus Macedo <matheusmacedo.dev@gmail.com>
2026-06-30 09:48:25 -0300

    Create ex05.c

---
+++ lista_exercicios/ex05.c
@@ -0,0 +1,14 @@
+// Ler um número e imprimir seu sucessor.
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    printf("Sucessor = %d\n", numero + 1);
+
+    return 0;
+}
