4a96ec925bed57dbd47c9f3b6ea225891e2f1386
Matheus Macedo <matheusmacedo.dev@gmail.com>
2026-06-30 10:11:23 -0300

    Update ex35.c

--- lista_exercicios/ex35.c
+++ lista_exercicios/ex35.c
@@ -1,1 +1,20 @@
 //  Ler o raio e a altura de um cilindro. Calcular e imprimir o volume desse cilindro
+
+#include <stdio.h>
+
+int main() {
+    float raio, altura, volume;
+    const float PI = 3.14159;
+
+    printf("Digite o raio do cilindro: ");
+    scanf("%f", &raio);
+
+    printf("Digite a altura do cilindro: ");
+    scanf("%f", &altura);
+
+    volume = PI * raio * raio * altura;
+
+    printf("Volume do cilindro = %.2f\n", volume);
+
+    return 0;
+}
