1869d978212446f70615adc1bd8a67d4314ba854
Matheus Macedo <matheusmacedo.dev@gmail.com>
2026-06-30 09:56:22 -0300

    Create ex15.c

---
+++ lista_exercicios/ex15.c
@@ -0,0 +1,20 @@
+//  Calcular e imprimir a média aritmética de 5 notas.
+
+#include <stdio.h>
+
+int main() {
+    float nota, soma = 0, media;
+
+    for(int i = 1; i <= 5; i++) {
+        printf("Digite a nota %d: ", i);
+        scanf("%f", &nota);
+
+        soma += nota;
+    }
+
+    media = soma / 5;
+
+    printf("Media = %.2f\n", media);
+
+    return 0;
+}
